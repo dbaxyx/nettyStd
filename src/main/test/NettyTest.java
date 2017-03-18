c1c602c8c9a283e10f139491357f54a3890a7995
dba_xyx <dba_xyx@hotmail.com>
2017-03-18 23:54:06 +0800

    增加单元测试、日志

--- src/main/test/NettyTest.java
+++ src/main/test/NettyTest.java
@@ -2,4 +2,9 @@
  * Created by dba on 2017/3/15.
  */
 public class NettyTest {
+    private static final Logger logger = LoggerFacotry.getLogger(NettyTest.class);
+    @Test
+    public void testNetty() {
+
+    }
 }
